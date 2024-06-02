20947372669de1c26fa832aa75edab0ee7a64a4f
sh <70614061+suh9023@users.noreply.github.com>
2024-06-02 20:04:22 +0900

    test plan grid

--- todo/apps.py
+++ todo/apps.py
@@ -1,6 +1,8 @@
 from django.apps import AppConfig
-
+from django.db.models.signals import post_migrate
 
 class TodoConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'todo'
+    def ready(self):
+        from . import signals
